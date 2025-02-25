805e24c0e76727507b7e120a80fc29a1c97a909b
Rufus Victor <139438518+RufusVictor@users.noreply.github.com>
2025-02-25 19:12:47 +0530

    Update TypeConversions.java

--- TypeConversions.java
+++ TypeConversions.java
@@ -6,32 +6,27 @@ public class TypeConversions {
         long longValue = intValue;
         float floatValue = longValue;
         double doubleValue = floatValue;
-
         System.out.println("Widening Conversions:");
         System.out.println("byte to short: " + shortValue);
         System.out.println("short to int: " + intValue);
         System.out.println("int to long: " + longValue);
         System.out.println("long to float: " + floatValue);
         System.out.println("float to double: " + doubleValue);
-
         double originalDouble = 99.99;
         float floatFromDouble = (float) originalDouble;
         long longFromFloat = (long) floatFromDouble;
         int intFromLong = (int) longFromFloat;
         short shortFromInt = (short) intFromLong;
         byte byteFromShort = (byte) shortFromInt;
-
         System.out.println("\nNarrowing Conversions:");
         System.out.println("double to float: " + floatFromDouble);
         System.out.println("float to long: " + longFromFloat);
         System.out.println("long to int: " + intFromLong);
         System.out.println("int to short: " + shortFromInt);
         System.out.println("short to byte: " + byteFromShort);
-
         char charValue = 'A';
         int intFromChar = charValue;
         char charFromInt = (char) intFromChar;
-
         System.out.println("\nCharacter Conversions:");
         System.out.println("char to int: " + intFromChar);
         System.out.println("int to char: " + charFromInt);
